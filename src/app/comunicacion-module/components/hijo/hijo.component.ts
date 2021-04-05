21f92e7de959344c4406b4962c72ee4d02812149
cristobal <c.cifuentes1990@gmail.com>
2021-04-05 00:51:33 -0400

    agregamos modulo de comunicacion

--- src/app/comunicacion-module/components/hijo/hijo.component.ts
+++ src/app/comunicacion-module/components/hijo/hijo.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Event } from '@angular/router';
+
 
 @Component({
   selector: 'app-hijo',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HijoComponent implements OnInit {
 
+  @Input() nombreHijo: string = "";
+  @Output() cambioNombreHijo = new EventEmitter<string>();
   constructor() { }
 
   ngOnInit() {
   }
+
+  cambiarNombre(){
+
+    this.cambioNombreHijo.emit(this.nombreHijo);
+  }
+
 
 }
--- src/app/comunicacion-module/comunicacion-module.module.ts
+++ src/app/comunicacion-module/comunicacion-module.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PadreComponent } from './components/padre/padre.component';
 import { HijoComponent } from './components/hijo/hijo.component';
+import { ComunicacionRoutingModule } from './comunicacion-routing.module';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [PadreComponent, HijoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ComunicacionRoutingModule,
+    FormsModule
   ]
 })
 export class ComunicacionModuleModule { }
